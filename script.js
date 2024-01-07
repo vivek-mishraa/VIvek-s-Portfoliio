9b5a2ca7f4d6526dbe327895cc42ec0f6201513c
vivek mishra <vivekmishra1322@gmail.com>
2024-01-07 15:34:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendMail(){
+
+    let parms={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        number: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+
+    }
+    emailjs.send("service_js1vpa1","template_cj3b8sd",parms).then(alert("Email Sent !"))
+}
