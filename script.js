ecde9d18cc0530df2e951e5f940938b0a8ab1738
vivek mishra <vivekmishra1322@gmail.com>
2024-01-19 15:13:23 +0530

    mail error solved

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function sendMail(){
     let parms={
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        number: document.getElementById("phone").value,
+        number: document.getElementById("number").value,
         message: document.getElementById("message").value,
 
     }
-    emailjs.send("service_js1vpa1","template_cj3b8sd",parms).then(alert("Email Sent !"))
+    emailjs.send("service_js1vpa1","template_cj3b8sd",parms).then(alert("Email Sent ! "))
 }
